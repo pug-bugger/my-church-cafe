6f7603313a2a3045bc3d7b3bd1a906679a753233
pug-bugger <wrongpugbug@gmail.com>
2025-10-14 15:48:51 +0300

    test 5

--- src/app/terminal/page.tsx
+++ src/app/terminal/page.tsx
@@ -11,6 +11,7 @@ export default function TerminalPage() {
       <Tabs defaultValue="drinks" className="w-full">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="drinks">Drinks</TabsTrigger>
+
           <TabsTrigger value="orders">Orders</TabsTrigger>
         </TabsList>
 
